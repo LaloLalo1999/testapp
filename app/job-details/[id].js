54d181b10e2a77010c0d139907fcfb11594b6ad8
LaloLalo1999 <eduardo.lalo1999@gmail.com>
2023-09-05 13:57:05 -0600

    Updated job page in [id].js

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,8 +1,42 @@
-import React from 'react'
+import { Text, View, SafeAreaView, ActivityIndicator, RefreshControl } from 'react-native'
+import { Stack, useRouter, useGlobalSearchParams } from 'expo-router'
+import { useCallback, useState } from 'react'
+
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components'
+import { COLORS, icons, SIZES } from '../../constants'
+import useFetch from '../../hook/useFetch'
 
 export const JobDetails = () => {
+  const params = useGlobalSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch('job-details', {
+    job_id: params.id
+  })
+
   return (
-    <Text>JobDetails</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen>
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn 
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn 
+              iconUrl={icons.share}
+              dimension="60%"
+            />
+          )
+        }}
+      </Stack.Screen>
+    </SafeAreaView>
   )
 }
 
