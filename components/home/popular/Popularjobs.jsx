0acbdb089858f26a7a427f44e8753c6472c8a252
LaloLalo1999 <eduardo.lalo1999@gmail.com>
2023-09-05 11:08:25 -0600

    Added dotenv, useFetch hook with JSearch API, and popularjobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -5,11 +5,25 @@ import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
 import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+import useFetch from '../../../hook/useFetch'
 
 const Popularjobs = () => {
-  const router = useRouter()
-  const isLoading = false
-  const error = false
+  const router = useRouter();
+
+  const { data, isLoading, error } = useFetch(
+    'search', {
+    query: 'React developer',
+    num_pages: 1
+  })
+
+    const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
+  // console.log(data)
 
   return (
     <View style={styles.container}>
@@ -31,6 +45,8 @@ const Popularjobs = () => {
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={(item) => item?.job_id}
