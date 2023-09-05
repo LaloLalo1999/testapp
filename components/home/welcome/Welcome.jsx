6388d8ec6bf6adcb571490f7a35b4aab75ee83d2
LaloLalo1999 <eduardo.lalo1999@gmail.com>
2023-09-04 19:23:19 -0600

    Updated Welcome.jsx with search buttons

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,8 +12,11 @@ import { useRouter } from 'expo-router'
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants'
 
+const jobTypes = ['Full-Time', 'Part-Time', 'Contractor']
+
 const Welcome = () => {
   const router = useRouter()
+  const [activeJobType, setActiveJobType] = useState('Full-Time')
 
   return (
     <View>
@@ -26,9 +29,32 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput 
             style={styles.searchInput}
-            
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
           />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList 
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+            >
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
       </View>
     </View>
   )
